0389d1ed28ac1a25b643c9cc4e9b589b0f467992
dooont <matthewmaung02@gmail.com>
2023-08-15 17:19:01 -0400

    appbar and avatar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Container maxWidth = {false}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Pokedex
+          </Typography>
+          <Avatar alt="Pokeball" src="/pokeballtransparent.png"/>
+        </Toolbar>
+      </AppBar>
+      </Container>
+    </React.Fragment>
   );
 }
+ //Where the Actual Coding Happens
 
+ 
 export default App;
