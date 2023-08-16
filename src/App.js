dcd7413ed68af67133a44fc928158a9fb0ac75d5
dooont <matthewmaung02@gmail.com>
2023-08-15 21:34:36 -0400

    prep for API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from '@mui/material/Container';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import axios from 'axios';
+
+
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]); //In "cuz ur stupid terms" use state makes pokemonData an array, and setPokemonData sets the values based on the api
+    
+  const pokemon = [
+      {
+        name: 'Bulbasaur',
+        id: 1,
+        type: "Grass"
+      },
+      {
+        name: 'Ivysaur',
+        id: 2,
+        type: ["Grass", "Poison"]
+    },
+      {name: 'Venusaur',
+      id: 3,
+      type: ["Grass", "Poison"]}
+  ];
+
   return (
     <React.Fragment>
       <Container maxWidth = {false}>
@@ -22,6 +47,21 @@ function App() {
           <Avatar alt="Pokeball" src="/pokeballtransparent.png"/>
         </Toolbar>
       </AppBar>
+      <Container>
+      <List>
+        {
+          pokemon.map(x => {
+            return ( //always have keys :D
+            <ListItem disablePadding key={x.id}>
+              <ListItemButton>
+                <ListItemText primary= {x.name} />
+              </ListItemButton>
+            </ListItem>
+            );
+          }) 
+        }
+        </List>
+        </Container>
       </Container>
     </React.Fragment>
   );
